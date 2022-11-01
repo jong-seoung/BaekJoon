f1af548a849924b0ca291b218a435687a6e4ebe6
beak jong seoung <jjong015189@gmail.com>
2022-11-01 10:54:57 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,11 +1,8 @@
-A = int(input())
-B = int(input())
-C = int(input())
-j = [0,1,2,3,4,5,6,7,8,9]
+a = int(input())
+b = int(input())
+c = int(input())
 
-result =A*B*C
-re = list(str(result))
+m = list(str(a * b * c))
 
-for i in j:
-    print(re.count(f"{i}"))
-    i+=1
+for i in range(10):
+    print(m.count(str(i)))
