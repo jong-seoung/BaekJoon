a81d23454fbd122932deadd1ff5e463a6dbeb27c
beak jong seoung <jjong015189@gmail.com>
2022-11-09 22:30:12 +0900

    [Silver V] Title: 소수 찾기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+a = list(map(int,input().split()))
+cnt = 0
+
+for i in a:
+    if i != 1:
+        for j in range(2,i):
+            if i % j == 0:
+                break
+        else:
+            cnt+=1
+print(cnt)
+
+
