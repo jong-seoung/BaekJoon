0746682bb9584c1477e4ad48c657feb30a9ad2eb
beak jong seoung <jjong015189@gmail.com>
2022-11-21 17:19:30 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 36188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,13 @@
-N,M = map(int,input().split())
-card = list(map(int,input().split())) 
-lst = []
-lst2 = []
+n,m = map(int,input().split())
+card = list(map(int,input().split()))
 
-for i in range(0,N-2):
-    for j in range(i+1,N-1):
-        for k in range(j+1,N):
-            cnt = card[i]+card[j]+card[k]
-            if M - cnt >= 0:
-                lst.append(cnt)
- 
+lst = []
+
+for i in range(0,n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            sum = card[i]+card[j]+card[k]
+            if sum <= m:
+                lst.append(sum)
+
 print(max(lst))
