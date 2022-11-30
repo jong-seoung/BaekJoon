bb4ced0fd23d53684cbfac6f1ac82fb402883d62
beak jong seoung <jjong015189@gmail.com>
2022-11-30 22:14:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -3,11 +3,11 @@ n = int(input())
 cnt = 0
 
 while(1):
-    if n % 5 == 0:
-        cnt += (n//5)
+    if n %5 ==0:
+        cnt+=(n//5)
         print(cnt)
         break
-    n -= 3
+    n-=3
     cnt += 1
     if n < 0:
         print(-1)
