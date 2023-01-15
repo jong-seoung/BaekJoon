4ae316aa66f1556337bd869226cd6940bb1c4f04
beak jong seoung <jjong015189@gmail.com>
2023-01-15 15:17:37 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 49420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,7 +1,11 @@
-N = int(input())
-A = set(map(int, input().split()))
-M = int(input())
-arr = list(map(int, input().split()))
+n = int(input())
+A = set(map(int,input().split()))
+m = int(input())
+num = list(map(int,input().split()))
 
-for num in arr:
-    print(1) if num in A else print(0)
+
+for i in num:
+    if i in A:
+        print(1)
+    else:
+        print(0)
