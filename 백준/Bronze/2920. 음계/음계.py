c98610b91da06b86253c723201ea0ba32d908ebd
beak jong seoung <jjong015189@gmail.com>
2023-07-27 15:28:14 +0900

    [Bronze II] Title: 음계, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,8 +1,11 @@
-a = list(map(int,input().split()))
-b = [1,2,3,4,5,6,7,8]
-if a == b :
+cnt = list(map(int,input().split()))
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+b = [8, 7, 6, 5, 4, 3, 2, 1]
+
+if cnt == a:
     print("ascending")
-elif a == list(reversed(b)):
+elif cnt == b:
     print("descending")
 else:
     print("mixed")
