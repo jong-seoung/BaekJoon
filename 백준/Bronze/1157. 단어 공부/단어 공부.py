55f01bf7026751dda2228f573297257d29e7676e
beak jong seoung <jjong015189@gmail.com>
2023-07-27 11:15:08 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,11 +1,11 @@
-a = input().upper()
-b = list(set(a))
-c = []
+word = input().upper()
+cnt = []
+word_list = list(set(word))
 
-for i in b:
-    d = a.count(i)
-    c.append(d)
-if c.count(max(c)) > 1:
+for i in word_list:
+    cnt.append(word.count(i))
+
+if cnt.count(max(cnt)) > 1:
     print("?")
 else:
-    print(b[c.index(max(c))])
+    print(word_list[cnt.index(max(cnt))])
