29240d04b69d2b9b2e8d77949708f14a52fa369d
beak jong seoung <jjong015189@gmail.com>
2023-07-28 10:19:57 +0900

    [Bronze III] Title: ACM 호텔, Time: 88 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,14 +1,18 @@
-a = int(input())
+import math
+test_cnt = int(input())
 
-for i in range(a):
-    h, w, n = map(int,input().split())
+for i in range(test_cnt):
+    h, w, n = map(int, input().split())
+    cnt = 0
 
-    c = 0
-    for j in range(n):
-        c+=1
-        for k in range(1,h+1):
-            if c<10:
-                k *= 10
-            n -= 1
-            if n == 0:
-                print(f"{k}{c}")
+    for j in range(1, w+1):
+        for k in range(1, h+1):
+            cnt += 1
+            if cnt == n:
+                if j < 10:
+                    print(f"{k}0{j}")
+                else:
+                    print(f"{k}{j}")
+                break
+
+
