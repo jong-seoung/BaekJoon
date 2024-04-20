7ea27d14be3cf445f6797cc11ae4f4ebbbb01418
beak jong seoung <jjong015189@gmail.com>
2024-04-20 17:15:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,30 +1,31 @@
 N = int(input())
-graph = [list(map(int, input())) for i in range(N)]
-
+graph = [list(map(int, input())) for _ in range(N)]
+visited = [[0 for _ in range(N)] for _ in range(N)]
+cnt = 0
+res = []
 
 def dfs(x, y):
-        dx = [0, 0, 0, -1, 1]
-        dy = [0, -1, 1, 0, 0]
+    graph[x][y]=0
+    res[cnt-1] += 1
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-        for i in range(5):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (0 <= nx < N) and (0 <= ny < N):
-                if graph[ny][nx] == 1:
-                    graph[ny][nx] = 0
-                    result[cnt-1] += 1
-                    dfs(nx, ny)
+    for i in range(4):
+        nx = dx[i]+x
+        ny = dy[i]+y
+        if 0 <= nx < N and 0 <= ny < N:
+            if graph[nx][ny] == 1:
+                dfs(nx, ny)
 
-cnt = 0
-result = []
 for i in range(N):
     for j in range(N):
-        if graph[j][i] == 1:
+        if graph[i][j] == 1:
             cnt += 1
-            result.append(0)
+            res.append(0)
             dfs(i, j)
 
 print(cnt)
-result.sort()
-for i in range(len(result)):
-    print(result[i])
+
+res.sort()
+for i in res:
+    print(i)
