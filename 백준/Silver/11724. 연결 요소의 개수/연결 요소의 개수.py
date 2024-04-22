a50b1dfc68ea178dd9cbf2f7d627ddae221c81ee
beak jong seoung <jjong015189@gmail.com>
2024-04-22 09:36:14 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 65272 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,28 +2,27 @@ import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
 
-graph = [[] for i in range(N+1)]
-result = [0] * (N+1)
+n, m =map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+
+def dfs(x):
+    visited[x] = 1
+
+    for i in graph[x]:
+        if visited[i] == 0:
+            dfs(i)
+
+for i in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
 cnt = 0
-
-
-def bfs(n):
-    result[n] = 1
-    for i in graph[n]:
-        if result[i] == 0:
-            bfs(i)
-
-
-for i in range(M):
-    a, b = map(int, input().split())
-    graph[a] += [b]
-    graph[b] += [a]
-
-for i in range(1, N+1):
-    if result[i] == 0:
-        bfs(i)
+for i in range(1,n+1):
+    if visited[i] == 0:
         cnt += 1
+        dfs(i)
 
 print(cnt)
