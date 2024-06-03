ffa0107022fef23c46f24d3bd031106046102ea1
beak jong seoung <jjong015189@gmail.com>
2024-06-03 13:36:00 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 608 ms, Memory: 38304 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -0,0 +1,21 @@
+num = int(input())
+ban = []
+same = [0] * num
+
+for i in range(num):
+    ban.append(list(map(int, input().split())))
+    same[i] = [0] * num
+
+for i in range(5):
+    for j in range(num):
+        for k in range(j+1, num):
+            if ban[j][i] == ban[k][i]:
+                same[k][j]=1
+                same[j][k]=1
+
+cnt = []
+
+for s in same:
+    cnt.append(s.count(1))
+
+print(cnt.index(max(cnt))+1)
