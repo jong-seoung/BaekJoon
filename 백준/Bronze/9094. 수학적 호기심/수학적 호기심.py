9ac3457ec98d190e1602077e65789ad9221d2687
beak jong seoung <jjong015189@gmail.com>
2024-10-25 15:11:35 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4700 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -0,0 +1,10 @@
+T=int(input())
+
+for i in range(T):
+    n, m = map(int, input().split())
+    cnt = 0
+    for j in range(1, n-1):
+        for k in range(j+1, n):
+            if (j**2 + k**2 + m) % (j*k) == 0:
+                cnt += 1
+    print(cnt)
