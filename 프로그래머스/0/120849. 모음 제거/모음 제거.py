c5411fb341255f8779202f569498d64c7ff899a8
beak jong seoung <jjong015189@gmail.com>
2025-01-20 15:05:59 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,16 @@
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i == "a":
+            pass
+        elif i == "e":
+            pass        
+        elif i == "i":
+            pass        
+        elif i == "o":
+            pass        
+        elif i == "u":
+            pass
+        else:
+            answer += i 
+    return answer
