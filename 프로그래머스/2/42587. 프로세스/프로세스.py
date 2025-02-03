44d7d23456774ec07ca25c4988dc83e8ea2d34cc
beak jong seoung <jjong015189@gmail.com>
2025-02-03 23:19:12 +0900

    [level 2] Title: 프로세스, Time: 4.93 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,17 @@
+def solution(priorities, location):
+    answer = 0
+    res = []
+    while len(priorities) > 0:
+        M = max(priorities)
+        queue = priorities.pop(0)
+        location -= 1
+        print(M, res, queue, priorities, location)
+        if M != queue:
+            priorities.append(queue)
+            if location == -1:
+                location = len(priorities) - 1
+        else:
+            res.append(queue)
+            if location == -1:
+                print('0end')
+                return len(res)
