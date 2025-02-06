bc66afb466aaa31e9c7eccf6ee2bb06687f2d3a0
beak jong seoung <jjong015189@gmail.com>
2025-02-06 19:14:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.07 ms
+메모리: 10 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 22일 20:27:42
+2025년 02월 06일 19:14:11
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,21 +1,22 @@
 def solution(genres, plays):
-    genre_play_count = {}
-    for genre, play in zip(genres, plays):
-        if genre not in genre_play_count:
-            genre_play_count[genre] = 0
-        genre_play_count[genre] += play
-
-    genre_songs = {}
-    for i, (genre, play) in enumerate(zip(genres, plays)):
-        if genre not in genre_songs:
-            genre_songs[genre] = []
-        genre_songs[genre].append((play, i))  
-
-    sorted_genres = sorted(genre_play_count.keys(), key=lambda g: genre_play_count[g], reverse=True)
-
     answer = []
-    for genre in sorted_genres:
-        songs = sorted(genre_songs[genre], key=lambda x: (-x[0], x[1]))
-        answer.extend([idx for _, idx in songs[:2]])
+
+    dic1 = {}
+    dic2 = {}
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        if g not in dic1:
+            dic1[g] = [(i, p)]
+        else:
+            dic1[g].append((i, p))
+
+        if g not in dic2:
+            dic2[g] = p
+        else:
+            dic2[g] += p
+    print(dic1, dic2)
+    for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
+        for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
+            answer.append(i)
 
     return answer
