1b8b862966d3af88a4f889fc1b3221f0ef6a4419
beak jong seoung <jjong015189@gmail.com>
2025-02-13 11:30:54 +0900

    [level 2] Title: 더 맵게, Time: 1652.65 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,15 @@
 import heapq
 
 def solution(scoville, K):
-    heapq.heapify(scoville)
     answer = 0
+    heapq.heapify(scoville)
     
     while scoville[0] < K:
         try:
-            hp1 = heapq.heappop(scoville)
-            hp2 = heapq.heappop(scoville)
-            heapq.heappush(scoville, hp1 + (hp2*2))
+            s1 = heapq.heappop(scoville)
+            s2 = heapq.heappop(scoville)
+            heapq.heappush(scoville, s1 + (s2 * 2))
             answer += 1
         except:
             return -1
-
     return answer
