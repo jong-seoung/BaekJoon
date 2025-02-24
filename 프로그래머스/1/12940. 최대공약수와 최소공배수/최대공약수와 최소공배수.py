1a0d9fa9b26e2bf2c2e8f64d961abc9fbf20cb59
beak jong seoung <jjong015189@gmail.com>
2025-02-24 13:56:00 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+def lcm(a, b):
+    return (a * b) / gcd(a, b) 
+
+def solution(n, m):
+    answer = [gcd(n,m), lcm(n,m)]
+    return answer
