accf37935d5d5b887f6f8f4c444215b3ad4dd3af
beak jong seoung <jjong015189@gmail.com>
2025-04-27 14:08:48 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
-        if (num1 == num2){
+        if(num1 == num2){
             answer = 1;
         }
         return answer;
