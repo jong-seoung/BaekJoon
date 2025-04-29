346ee27be53c880394bbf513cc563014756074ee
beak jong seoung <jjong015189@gmail.com>
2025-04-29 20:17:30 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        answer = message.length() * 2;
+        int answer = message.length() * 2;
         return answer;
     }
 }
