27256a15e2ac1fce4addef6b9406efb5c5129f48
beak jong seoung <jjong015189@gmail.com>
2025-04-29 16:45:17 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.11 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+import java.lang.StringBuffer;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer sb = new StringBuffer(my_string);
+        String answer = sb.reverse().toString();
+        return answer;
+    }
+}
